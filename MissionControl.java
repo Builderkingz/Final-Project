bc9985936f5e9b0eadb67c418b2d26f0b08eaad5
Tman-is-cbooyl <t1m1a1n4265@gmail.com>
2025-02-21 18:00:58 -0500

    qee

--- MissionControl.java
+++ MissionControl.java
@@ -82,29 +82,29 @@ public class MissionControl {
                         }
                     } while (choice != 4);
                     break;
-                // // case 3:
-                // //     do {
-                // //         // Display menu options
-                // //         System.out.println("\n Menu");
-                // //         System.out.println("1. Launch");
-                // //         System.out.println("2. remove assinged astronauts");
-                // //         System.out.println("3. ");
-                // //         System.out.println("4. Quit");
-                // //         System.out.print("\n Enter your choice (1-4): ");
+                // case 3:
+                //     do {
+                //         // Display menu options
+                //         System.out.println("\n Menu");
+                //         System.out.println("1. Launch");
+                //         System.out.println("2. remove assinged astronauts");
+                //         System.out.println("3. ");
+                //         System.out.println("4. Quit");
+                //         System.out.print("\n Enter your choice (1-4): ");
 
-                // //         // Get user choice
-                // //         choice = input.nextInt();
+                //         // Get user choice
+                //         choice = input.nextInt();
 
-                // //         // Validate input range
-                // //         if (choice > 4 || choice < 1) {
-                // //             System.out.println("Invalid choice. Please enter a number between 1 and 4.");
-                // //         }
-                // //         input.nextLine(); // Consume newline
-                // //         switch (choice) {
-                // //             case 1:
+                //         // Validate input range
+                //         if (choice > 4 || choice < 1) {
+                //             System.out.println("Invalid choice. Please enter a number between 1 and 4.");
+                //         }
+                //         input.nextLine(); // Consume newline
+                //         switch (choice) {
+                //             case 1:
 
-                // //         }
-                // //         break;
+                //         }
+                //         break;
                 //     } while (choice != 4);
                 //     break;
             }
